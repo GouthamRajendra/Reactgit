663f2df93a178e3826596904fcae7bab6b48207a
Goutham <gouthamrajendra.neo@gmail.com>
2024-10-06 12:38:00 +0530

    jsdbfkj

--- src/praveen/one.js
+++ src/praveen/one.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const One = () => {
+    const [name,setName]=useState("na")
   return (
     <div>
-        <h1>Praveen branch is here</h1>
+        <h1>Praveen branch is here {name}</h1>
+        <h1 onClick={()=>setName("Goutham")}>ClickerHere</h1>
     </div>
   )
 }
