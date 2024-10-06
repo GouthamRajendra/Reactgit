84f9e4a750bbfaa91824ae0478fc615ec3e1a2a6
Goutham <gouthamrajendra.neo@gmail.com>
2024-10-06 12:40:09 +0530

    new

--- src/praveen/one.js
+++ src/praveen/one.js
@@ -1,13 +1,1 @@
-import React, { useState } from 'react'
-
-const One = () => {
-    const [name,setName]=useState("na")
-  return (
-    <div>
-        <h1>Praveen branch is here {name}</h1>
-        <h1 onClick={()=>setName("Goutham")}>ClickerHere</h1>
-    </div>
-  )
-}
-
-export default One
+rag
